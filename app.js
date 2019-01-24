17fa12e688cb8e2d734aef154e2840b26e5f26bf
khaledbnmohamed <34178030+khaledbnmohamed@users.noreply.github.com>
2019-01-25 01:15:08 +0200

    changed domain name on heroku

--- app.js
+++ app.js
@@ -17,6 +17,10 @@ const
   express = require('express'),
   https = require('https'),
   request = require('request');
+  imagur = require('last-fm')
+  imagur_key = new imagur('////YOUR LAST_FM API KEY/////', {
+  userAgent: 'DemoApp/1.0.0 (////DOMAIN NAME OF THE SERVER////)'
+})
 
 var app = express();
 app.set('port', process.env.PORT || 5000);
