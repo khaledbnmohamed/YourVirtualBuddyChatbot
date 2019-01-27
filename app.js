82a67c3408eea243a792eef4e512324277b29855
khaledbnmohamed <34178030+khaledbnmohamed@users.noreply.github.com>
2019-01-27 09:11:32 +0200

    Sending Image directly after succ fetching 3

--- app.js
+++ app.js
@@ -950,7 +950,7 @@ var req = https.request(options, function (res) {
 
     res.on("end", function (chunk) {
       var body = Buffer.concat(chunks);
-      console.log(formingElements(result));
+      sendImageMessage(senderId,formingElements(body));
 
     });
 
