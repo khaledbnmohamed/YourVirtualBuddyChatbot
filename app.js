ea13baa99daf74b9d080af525e02426cc0fcd106
khaledbnmohamed <34178030+khaledbnmohamed@users.noreply.github.com>
2019-01-28 05:21:44 +0200

    add welcome messages

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
-/*
- * Copyright 2016-present, Facebook, Inc.
- * All rights reserved.
- *
- * This source code is licensed under the license found in the
- * LICENSE file in the root directory of this source tree.
- *
- */
 
-/* jshint node: true, devel: true */
+
+//////////TODO add media filtration to extract videos out of images
 'use strict';
 
 const
