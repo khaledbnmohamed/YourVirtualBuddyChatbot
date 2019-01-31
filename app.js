1f174513f3ff482833d4286d6107ea376a712967
khaledbnmohamed <34178030+khaledbnmohamed@users.noreply.github.com>
2019-02-01 01:16:02 +0200

    added file for send functions 1

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ const
   express = require('express'),
   https = require('https'),
   request = require('request'),
-  fs = require('fs');
+  fs = require('fs'),
+  tools = require('./sendFunctions.js');
+
 
 
 
@@ -258,7 +260,7 @@ function receivedAuthentication(event) {
 
   // When an authentication is received, we'll send a message back to the sender
   // to let them know it was successful.
-  sendTextMessage(senderID, "Authentication successful");
+  tools.sendTextMessage(senderID, "Authentication successful");
 }
 
 /*
@@ -304,7 +306,7 @@ function receivedMessage(event) {
     var quickReplyPayload = quickReply.payload;
     console.log("Quick reply for message %s with payload %s",
       messageId, quickReplyPayload);
-    sendTypingOn(senderID); //typing on till fetching
+    tools.sendTypingOn(senderID); //typing on till fetching
 
 	switch (quickReplyPayload) {
 	case 'personal_account_memes':
@@ -328,10 +330,10 @@ function receivedMessage(event) {
         break;
 
      case 'do nothing':
-         sendTypingOff(senderID);
+         tools.sendTypingOff(senderID);
 	 	 console.log(" I CHOSE do nothing");
 	   	 saveToFile(null,null,false);
-	     sendTextMessage(senderID,"Whatever you want <3 ")
+	     tools.sendTextMessage(senderID,"Whatever you want <3 ")
 
 
         break;
@@ -360,15 +362,15 @@ function receivedMessage(event) {
       switch (messageText.replace(/[^\w\s]/gi, '').trim().toLowerCase()) {
         case 'hello':
         case 'hi':
-          sendHiMessage(senderID);
+          tools.sendHiMessage(senderID);
           break;
 
         case 'image':
-          requiresServerURL(sendImageMessage, [senderID]);
+          requiresServerURL(tools.sendImageMessage, [senderID]);
           break;
 
         case 'gif':
-          requiresServerURL(sendGifMessage, [senderID]);
+          requiresServerURL(tools.sendGifMessage, [senderID]);
           break;
 
         case 'audio':
@@ -384,32 +386,32 @@ function receivedMessage(event) {
           break;
 
         case 'button':
-          sendButtonMessage(senderID);
+          tools.sendButtonMessage(senderID);
           break;
 
         case 'generic':
-          requiresServerURL(sendGenericMessage, [senderID]);
+          requiresServerURL(tools.sendGenericMessage, [senderID]);
           break;
 
         case 'receipt':
-          requiresServerURL(sendReceiptMessage, [senderID]);
+          requiresServerURL(tools.sendReceiptMessage, [senderID]);
           break;
 
         case 'memes':
-          sendQuickReply(senderID);
+          tools.sendQuickReply(senderID);
 		  // checkToSendMore(senderID);
           break;
 
         case 'read receipt':
-          sendReadReceipt(senderID);
+          tools.sendReadReceipt(senderID);
           break;
 
         case 'typing on':
-          sendTypingOn(senderID);
+          tools.sendTypingOn(senderID);
           break;
 
         case 'typing off':
-          sendTypingOff(senderID);
+          tools.sendTypingOff(senderID);
           break;
 
         case 'account linking':
@@ -417,7 +419,7 @@ function receivedMessage(event) {
           break;
 
         case 'send meme':
-          // sendTypingOn(senderID); //typing on till fetching
+          // tools.sendTypingOn(senderID); //typing on till fetching
           saveToFile(2,"memes",true);
           chooseCaller(2,null,senderID); 
 		  checkToSendMore(senderID);
@@ -425,10 +427,10 @@ function receivedMessage(event) {
           break;
 
         default:
-          sendTextMessage(senderID, default_text);
+          tools.sendTextMessage(senderID, default_text);
       }
     } else if (messageAttachments) {
-      sendTextMessage(senderID, "Message with attachment received");
+      tools.sendTextMessage(senderID, "Message with attachment received");
     }
   }
 
@@ -481,7 +483,7 @@ function receivedMessage(event) {
 
     // When a postback is called, we'll send a message back to the sender to
     // let them know it was successful
-    sendTextMessage(senderID, "Postback called");
+    tools.sendTextMessage(senderID, "Postback called");
   }
 
   /*
@@ -549,503 +551,7 @@ Once you've finished these steps, try typing “video” or “image”.
     }
   }
 
-  function sendHiMessage(recipientId) {
-    var messageData = {
-      recipient: {
-        id: recipientId
-      },
-      message: {
-        text: `
-Hi,
 
-I'm here to help you on your bad days. Try out "memes", "send meme" to see magic (SPOILER:it'll be a meme). More functionalities are to come
-
-I really hope one day, You'll find the right person to forward these memes to <3 
-      `
-      }
-    }
-
-    callSendAPI(messageData);
-  }
-
-  /*
-   * Send an image using the Send API.
-   *
-   */
-  function sendImageMessage(recipientId, image_message_url) {
-    var messageData = {
-      recipient: {
-        id: recipientId
-      },
-      message: {
-        attachment: {
-          type: "image",
-          payload: {
-            url: image_message_url
-          }
-        }
-      }
-    };
-
-    callSendAPI(messageData);
-  }
-
-
-  /*
-   * Send a meme using the Send API.
-   *
-   */
-  function sendMemeMessage(recipientId) {
-    // var Search_query = messageText;
-    // ImageLink= fetchingData_from_gallery_searchAPi(senderID);
-
-    var messageData = {
-      recipient: {
-        id: recipientId
-      },
-      message: {
-        attachment: {
-          type: "image",
-          payload: {
-            url: ImageLink
-          }
-        }
-      }
-    };
-
-    callSendAPI(messageData);
-
-  }
-  /*
-   * Send a Gif using the Send API.
-   *
-   */
-  function sendGifMessage(recipientId) {
-    var messageData = {
-      recipient: {
-        id: recipientId
-      },
-      message: {
-        attachment: {
-          type: "image",
-          payload: {
-            url: SERVER_URL + "/assets/instagram_logo.gif"
-          }
-        }
-      }
-    };
-
-    callSendAPI(messageData);
-  }
-
-  /*
-   * Send audio using the Send API.
-   *
-   */
-  function sendAudioMessage(recipientId) {
-    var messageData = {
-      recipient: {
-        id: recipientId
-      },
-      message: {
-        attachment: {
-          type: "audio",
-          payload: {
-            url: SERVER_URL + "/assets/sample.mp3"
-          }
-        }
-      }
-    };
-
-    callSendAPI(messageData);
-  }
-
-  /*
-   * Send a video using the Send API.
-   *
-   */
-  function sendVideoMessage(recipientId) {
-    var messageData = {
-      recipient: {
-        id: recipientId
-      },
-      message: {
-        attachment: {
-          type: "video",
-          payload: {
-            url: SERVER_URL + "/assets/allofus480.mov"
-          }
-        }
-      }
-    };
-
-    callSendAPI(messageData);
-  }
-
-  /*
-   * Send a file using the Send API.
-   *
-   */
-  function sendFileMessage(recipientId) {
-    var messageData = {
-      recipient: {
-        id: recipientId
-      },
-      message: {
-        attachment: {
-          type: "file",
-          payload: {
-            url: SERVER_URL + "/assets/test.txt"
-          }
-        }
-      }
-    };
-
-    callSendAPI(messageData);
-  }
-
-  /*
-   * Send a text message using the Send API.
-   *
-   */
-  function sendTextMessage(recipientId, messageText) {
-    var messageData = {
-      recipient: {
-        id: recipientId
-      },
-      message: {
-        text: messageText,
-        metadata: "DEVELOPER_DEFINED_METADATA"
-      }
-    };
-
-    callSendAPI(messageData);
-  }
-
-  /*
-   * Send a button message using the Send API.
-   *
-   */
-  function sendButtonMessage(recipientId) {
-    var messageData = {
-      recipient: {
-        id: recipientId
-      },
-      message: {
-        attachment: {
-          type: "template",
-          payload: {
-            template_type: "button",
-            text: "This is test text",
-            buttons: [{
-              type: "web_url",
-              url: "https://www.oculus.com/en-us/rift/",
-              title: "Open Web URL"
-            }, {
-              type: "postback",
-              title: "Trigger Postback",
-              payload: "DEVELOPER_DEFINED_PAYLOAD"
-            }, {
-              type: "phone_number",
-              title: "Call Phone Number",
-              payload: "+16505551234"
-            }]
-          }
-        }
-      }
-    };
-
-    callSendAPI(messageData);
-  }
-
-  /*
-   * Send a Structured Message (Generic Message type) using the Send API.
-   *
-   */
-  function sendGenericMessage(recipientId) {
-    var messageData = {
-      recipient: {
-        id: recipientId
-      },
-      message: {
-        attachment: {
-          type: "template",
-          payload: {
-            template_type: "generic",
-            elements: [{
-              title: "rift",
-              subtitle: "Next-generation virtual reality",
-              item_url: "https://www.oculus.com/en-us/rift/",
-              image_url: SERVER_URL + "/assets/rift.png",
-              buttons: [{
-                type: "web_url",
-                url: "https://www.oculus.com/en-us/rift/",
-                title: "Open Web URL"
-              }, {
-                type: "postback",
-                title: "Call Postback",
-                payload: "Payload for first bubble",
-              }],
-            }, {
-              title: "touch",
-              subtitle: "Your Hands, Now in VR",
-              item_url: "https://www.oculus.com/en-us/touch/",
-              image_url: SERVER_URL + "/assets/touch.png",
-              buttons: [{
-                type: "web_url",
-                url: "https://www.oculus.com/en-us/touch/",
-                title: "Open Web URL"
-              }, {
-                type: "postback",
-                title: "Call Postback",
-                payload: "Payload for second bubble",
-              }]
-            }]
-          }
-        }
-      }
-    };
-
-    callSendAPI(messageData);
-  }
-
-  /*
-   * Send a receipt message using the Send API.
-   *
-   */
-  function sendReceiptMessage(recipientId) {
-    // Generate a random receipt ID as the API requires a unique ID
-    var receiptId = "order" + Math.floor(Math.random() * 1000);
-
-    var messageData = {
-      recipient: {
-        id: recipientId
-      },
-      message: {
-        attachment: {
-          type: "template",
-          payload: {
-            template_type: "receipt",
-            recipient_name: "Peter Chang",
-            order_number: receiptId,
-            currency: "USD",
-            payment_method: "Visa 1234",
-            timestamp: "1428444852",
-            elements: [{
-              title: "Oculus Rift",
-              subtitle: "Includes: headset, sensor, remote",
-              quantity: 1,
-              price: 599.00,
-              currency: "USD",
-              image_url: SERVER_URL + "/assets/riftsq.png"
-            }, {
-              title: "Samsung Gear VR",
-              subtitle: "Frost White",
-              quantity: 1,
-              price: 99.99,
-              currency: "USD",
-              image_url: SERVER_URL + "/assets/gearvrsq.png"
-            }],
-            address: {
-              street_1: "1 Hacker Way",
-              street_2: "",
-              city: "Menlo Park",
-              postal_code: "94025",
-              state: "CA",
-              country: "US"
-            },
-            summary: {
-              subtotal: 698.99,
-              shipping_cost: 20.00,
-              total_tax: 57.67,
-              total_cost: 626.66
-            },
-            adjustments: [{
-              name: "New Customer Discount",
-              amount: -50
-            }, {
-              name: "$100 Off Coupon",
-              amount: -100
-            }]
-          }
-        }
-      }
-    };
-
-    callSendAPI(messageData);
-  }
-
-  /*
-   * Send a message with Quick Reply buttons.
-   *
-   */
-  function sendQuickReply(recipientId) {
-    var messageData = {
-      recipient: {
-        id: recipientId
-      },
-      message: {
-        text: "What's your favorite category?",
-        quick_replies: [
-          {
-            "content_type": "text",
-            "title": "Sad Memes",
-            "payload": "sad memes"
-          },
-          {
-            "content_type": "text",
-            "title": "Dunk Memes",
-            "payload": "dunk memes"
-          },
-          {
-            "content_type": "text",
-            "title": "Love memes",
-            "payload": "love memes"
-          },
-          {
-            "content_type": "text",
-            "title": "Surprise Me !",
-            "payload": "personal_account_memes"
-          }
-        ]
-      }
-    };
-
-    callSendAPI(messageData);
-  }
-
-
-  function SendMore(recipientId) {
-    var messageData = {
-      recipient: {
-        id: recipientId
-      },
-      message: {
-        text: "More from the same category?",
-        quick_replies: [
-          {
-            "content_type": "text",
-            "title": "Yes",
-            "payload": "send_alike"
-          },
-          {
-            "content_type": "text",
-            "title": "No",
-            "payload": "do nothing"
-          }
-        ]
-      }
-    };
-    callSendAPI(messageData);
-  }
-
-  /*
-   * Send a read receipt to indicate the message has been read
-   *
-   */
-  function sendReadReceipt(recipientId) {
-    console.log("Sending a read receipt to mark message as seen");
-
-    var messageData = {
-      recipient: {
-        id: recipientId
-      },
-      sender_action: "mark_seen"
-    };
-
-    callSendAPI(messageData);
-  }
-
-  /*
-   * Turn typing indicator on
-   *
-   */
-  function sendTypingOn(recipientId) {
-    console.log("Turning typing indicator on");
-
-    var messageData = {
-      recipient: {
-        id: recipientId
-      },
-      sender_action: "typing_on"
-    };
-
-    callSendAPI(messageData);
-  }
-
-  /*
-   * Turn typing indicator off
-   *
-   */
-  function sendTypingOff(recipientId) {
-    console.log("Turning typing indicator off");
-
-    var messageData = {
-      recipient: {
-        id: recipientId
-      },
-      sender_action: "typing_off"
-    };
-
-    callSendAPI(messageData);
-  }
-
-  /*
-   * Send a message with the account linking call-to-action
-   *
-   */
-  function sendAccountLinking(recipientId) {
-    var messageData = {
-      recipient: {
-        id: recipientId
-      },
-      message: {
-        attachment: {
-          type: "template",
-          payload: {
-            template_type: "button",
-            text: "Welcome. Link your account.",
-            buttons: [{
-              type: "account_link",
-              url: SERVER_URL + "/authorize"
-            }]
-          }
-        }
-      }
-    };
-
-    callSendAPI(messageData);
-  }
-
-  /*
-   * Call the Send API. The message data goes in the body. If successful, we'll
-   * get the message id in a response
-   *
-   */
-  function callSendAPI(messageData) {
-    request({
-      uri: 'https://graph.facebook.com/v2.6/me/messages',
-      qs: { access_token: PAGE_ACCESS_TOKEN },
-      method: 'POST',
-      json: messageData
-
-    }, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        var recipientId = body.recipient_id;
-        var messageId = body.message_id;
-
-        if (messageId) {
-          console.log("Successfully sent message with id %s to recipient %s",
-            messageId, recipientId);
-        } else {
-          console.log("Successfully called Send API for recipient %s",
-            recipientId);
-        }
-      } else {
-        console.error("Failed calling Send API", response.statusCode, response.statusMessage, body.error);
-      }
-    });
-  }
 
 
 
@@ -1094,8 +600,8 @@ I really hope one day, You'll find the right person to forward these memes to <3
         let image_link = formingElements(body, senderID, false)
         if (image_link) {
 
-          sendTypingOff(senderID);
-          sendImageMessage(senderID, image_link);
+          tools.sendTypingOff(senderID);
+          tools.sendImageMessage(senderID, image_link);
 
 
         }
@@ -1146,8 +652,8 @@ I really hope one day, You'll find the right person to forward these memes to <3
         let image_link = formingElements(body, senderID, true)
         if (image_link) {
 
-          sendTypingOff(senderID);
-          sendImageMessage(senderID, image_link);
+          tools.sendTypingOff(senderID);
+          tools.sendImageMessage(senderID, image_link);
 
 
         }
@@ -1209,7 +715,7 @@ I really hope one day, You'll find the right person to forward these memes to <3
       }
     }
 
-    sendTextMessage(senderID, "No memes for you today go get a life")
+    tools.sendTextMessage(senderID, "No memes for you today go get a life")
     return;
   }
 
@@ -1244,6 +750,10 @@ if (fileObject.want_more ){
 
 
 }
+}
+
+function firstEntity(nlp, name) {
+  return nlp && nlp.entities && nlp.entities[name] && nlp.entities[name][0];
 }
 
 function saveToFile(number,word,want_more){
