e0f2259a79b9715ba2d678d392bbc5956cd53b13
khaledbnmohamed <34178030+khaledbnmohamed@users.noreply.github.com>
2019-01-31 09:28:20 +0200

    delete more inside call fucntion

--- app.js
+++ app.js
@@ -1241,7 +1241,7 @@ function checkToSendMore(senderID){
 
 if (fileObject.want_more ){
     
-      setTimeout(function(){SendMore(senderID)},7000); //must be called like that   why ? https://stackoverflow.com/a/5520159/5627553
+      setTimeout(function(){SendMore(senderID)},5000); //must be called like that   why ? https://stackoverflow.com/a/5520159/5627553
 
 
 }
