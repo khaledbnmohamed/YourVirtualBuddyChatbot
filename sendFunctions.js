0433298780052b8b510a66e64eb736d103801188
khaledbnmohamed <34178030+khaledbnmohamed@users.noreply.github.com>
2019-02-01 01:48:57 +0200

    added required files

--- sendFunctions.js
+++ sendFunctions.js
@@ -10,6 +10,29 @@ config = require('config'),
 crypto = require('crypto'),
 express = require('express');
 
+var ImageLink = 'https://i.imgur.com/KZC2CW9.jpg'
+var clientId = '8056e5db3f369d1'
+var imgur_access_token = '2a8f6dacd57b657d8f9542b166724964c1ed8f8f'
+var imgur_username = 'khaledbnmohamed'
+var FirstQuery = true;
+let counter = 0;
+var default_text = [ "You know that no matter how cool I am to you,",
+  " at the end I'm a preprogrammed meme sender so please don't ask me for neither commitment or Anything I don't understand.",
+  " Just type SEND MEME"
+].join('\n');
+ 
+
+var app = express();
+
+
+var fileObject =JSON.parse(fs.readFileSync('./inputMemory.json', 'utf8'));
+
+app.set('port', process.env.PORT || 5000);
+app.set('view engine', 'ejs');
+app.use(bodyParser.json({ verify: verifyRequestSignature }));
+app.use(express.static('public'));
+
+
 module.exports = 
 {
     /*
