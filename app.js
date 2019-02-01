4ca6ac4fcbfd336d564737ee0f32f34b8cf46363
khaledbnmohamed <34178030+khaledbnmohamed@users.noreply.github.com>
2019-02-01 14:06:08 +0200

    bug fix

--- app.js
+++ app.js
@@ -408,7 +408,7 @@ function receivedMessage(event) {
     return;
   } else if (quickReply) {
     var quickReplyPayload = quickReply.payload;
-    console.log("Quick reply for message %s with payload %s",
+    // console.log("Quick reply for message %s with payload %s",
       messageId, quickReplyPayload);
     tools.sendTypingOn(senderID); //typing on till fetching
 
@@ -560,12 +560,12 @@ function receivedMessage(event) {
 
     if (messageIDs) {
       messageIDs.forEach(function (messageID) {
-        console.log("Received delivery confirmation for message ID: %s",
-          messageID);
+        // console.log("Received delivery confirmation for message ID: %s",
+        //   messageID);
       });
     }
 
-    console.log("All message before %d were delivered.", watermark);
+    // console.log("All message before %d were delivered.", watermark);
   }
 
 
@@ -603,8 +603,8 @@ function receivedMessage(event) {
                                 //sendMessage(event.sender.id,msg);   
                                 });   
         } 
-    console.log("Received postback for user %d and page %d with payload '%s' " +
-      "at %d", senderID, recipientID, payload, timeOfPostback);
+    // console.log("Received postback for user %d and page %d with payload '%s' " +
+    //   "at %d", senderID, recipientID, payload, timeOfPostback);
 
     // When a postback is called, we'll send a message back to the sender to
     // let them know it was successful
@@ -626,8 +626,8 @@ function receivedMessage(event) {
     var watermark = event.read.watermark;
     var sequenceNumber = event.read.seq;
 
-    console.log("Received message read event for watermark %d and sequence " +
-      "number %d", watermark, sequenceNumber);
+    // console.log("Received message read event for watermark %d and sequence " +
+    //   "number %d", watermark, sequenceNumber);
   }
 
   /*
@@ -645,8 +645,8 @@ function receivedMessage(event) {
     var status = event.account_linking.status;
     var authCode = event.account_linking.authorization_code;
 
-    console.log("Received account link event with for user %d with status %s " +
-      "and auth code %s ", senderID, status, authCode);
+    // console.log("Received account link event with for user %d with status %s " +
+    //   "and auth code %s ", senderID, status, authCode);
   }
 
   /*
@@ -706,12 +706,10 @@ function receivedMessage(event) {
         
         image_link = formingElements(body, senderID, false)
 
-        }
+          }
       else{
         //Handling empty image responses 
-
-          tools.sendTypingOff(senderID);
-          tools.sendImageMessage(senderID, image_link);
+        tools.sendTextMessage(senderID,"Please try agian :) ")
 
       }
 
@@ -829,6 +827,8 @@ function receivedMessage(event) {
 
          while (!parsed.data[i].link)
               {
+
+                console.log("I enetered here")
 
               i++;
 
