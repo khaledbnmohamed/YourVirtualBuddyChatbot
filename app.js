287637e1715ed50b79ed989b39b7de5c32ce3f3e
khaledbnmohamed <34178030+khaledbnmohamed@users.noreply.github.com>
2019-02-04 17:19:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -233,7 +233,7 @@ console.log("Entered "+ req.body.result);
     // You must send back a 200, within 20 seconds, to let us know you've
     // successfully received the callback. Otherwise, the request will time out.
     res.sendStatus(200);
-  }
+  
 });
 
 
