76ad59f8cf1a933a99dee0df6f690e4e81451fc2
Khaled Awad <khaledbnmohamed@hotmail.com>
2019-11-18 16:51:11 +0200

    Updat sql file

--- init.sql
+++ init.sql
@@ -1,8 +1,33 @@
+DROP TABLE IF EXISTS users, gallery_memes, account_memes, memes_sent
+CASCADE;
+
 CREATE TABLE users (
-  ID SERIAL PRIMARY KEY,
-  memes_no INTEGER NOT NULL,
-  memes_links VARCHAR(255) NOT NULL
+  id SERIAL ,
+  fb_id VARCHAR PRIMARY KEY,
+  rec_images int,
+  created_at TIMESTAMP
+);
+CREATE TABLE gallery_memes (
+    id SERIAL ,
+    imgur_id VARCHAR PRIMARY KEY,
+    score int,
+    created_at TIMESTAMP
+);
+CREATE TABLE account_memes (
+    id SERIAL,
+    imgur_id VARCHAR PRIMARY KEY,
+    score int,
+    created_at TIMESTAMP
 );
 
-INSERT INTO users (memes_no, memes_links)
-VALUES  (15, 'linkyoooo');
+CREATE TABLE memes_sent(
+    fb_id VARCHAR REFERENCES users,
+    imgur_id_gallery VARCHAR REFERENCES gallery_memes(imgur_id),
+    imgur_id_account VARCHAR REFERENCES account_memes(imgur_id),
+    PRIMARY KEY (fb_id, imgur_id_gallery,imgur_id_account)
+);
+INSERT INTO users (fb_id,rec_images)
+VALUES  ('15kdas1',1);
+
+INSERT INTO gallery_memes (imgur_id, score)
+VALUES  ('imgur_is', 15644);
