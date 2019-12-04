029f4f1dee93566e7ab078a54e601adc5cd835b3
Khaled Awad <khaledbnmohamed@hotmail.com>
2019-12-04 15:50:42 +0200

    Adding staging config

--- config/config.js
+++ config/config.js
@@ -1,7 +1,8 @@
 require('dotenv').config()
 
 const { Pool } = require('pg')
-const isProduction = process.env.NODE_ENV === 'production'
+const isProduction = process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'staging'
+
 
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
 console.log("connection string is " + connectionString)
