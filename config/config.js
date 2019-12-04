9b1f271abc415c4f95a0a4805b1f803b2dc95e46
Khaled Awad <khaledbnmohamed@hotmail.com>
2019-12-04 20:21:33 +0200

    logging

--- config/config.js
+++ config/config.js
@@ -4,11 +4,11 @@ const isProduction = process.env.NODE_ENV === 'production' || process.env.NODE_E
 
 
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
-console.log("connection string is " + connectionString)
 
 const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
   ssl: isProduction,
 })
+console.log("connection string is " + connectionString)
 
 module.exports = { pool }
