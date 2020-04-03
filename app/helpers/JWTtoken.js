82819d465fd0e1805c2443888635a3e36cbbbd09
Khaled Awad <khaledbnmohamed@hotmail.com>
2020-04-03 18:32:54 +0200

    #8: update files for remocing senstive data

--- app/helpers/JWTtoken.js
+++ app/helpers/JWTtoken.js
@@ -1,39 +1,34 @@
-const fs   = require('fs');
-const jwt   = require('jsonwebtoken');
+const fs = require('fs');
+const jwt = require('jsonwebtoken');
 
 // use 'utf8' to get string instead of byte array  (512 bit key)
-var privateKEY  = fs.readFileSync('./PrivateKey.key', 'utf8');
+const privateKey = fs.readFileSync('./config/PrivateKey.key', 'utf8');
 
 module.exports = {
 
- sign: () => {
-
- 
-var payload = {
- data1: "Data 1",
- data2: "Data 2",
- data3: "Data 3",
- data4: "Data 4",
-};   
+  sign: () => {
+    const payload = {
+      data1: 'Data 1',
+      data2: 'Data 2',
+      data3: 'Data 3',
+      data4: 'Data 4',
+    };
 
 
+    // Token signing options
+    const signOptions = {
+      issuer: 'dialogflowversion2@myvirtualbuddy-fab9e.iam.gserviceaccount.com',
+      subject: 'dialogflowversion2@myvirtualbuddy-fab9e.iam.gserviceaccount.com',
+      audience: 'https://dialogflow.googleapis.com/google.cloud.dialogflow.v2beta1.Sessions',
+      expiresIn: '1h', // 30 days validity
+      algorithm: 'RS256',
+      keyid: '4285b9a752db1057b238729826e15e030c2824fb',
+    };
+    // console.log(jwt.sign(payload, privateKey, signOptions))
+    return (jwt.sign(payload, privateKey, signOptions));
+  },
 
-   // Token signing options
-  var signOptions = {
-      issuer:  "dialogflowversion2@myvirtualbuddy-fab9e.iam.gserviceaccount.com",
-      subject: "dialogflowversion2@myvirtualbuddy-fab9e.iam.gserviceaccount.com",
-      audience:  "https://dialogflow.googleapis.com/google.cloud.dialogflow.v2beta1.Sessions",
-      expiresIn:  "1h",    // 30 days validity
-      algorithm:  "RS256",
-      keyid :"4285b9a752db1057b238729826e15e030c2824fb"    
-  };
-	// console.log(jwt.sign(payload, privateKEY, signOptions))
-  return (jwt.sign(payload, privateKEY, signOptions));
-},
+  decode: (token) => jwt.decode(token, { complete: true }),
+  // returns null if token is invalid
 
- decode: (token) => {
-    return jwt.decode(token, {complete: true});
-    //returns null if token is invalid
- }
-}
-
+};
--- app/helpers/helpingFunctions.js
+++ app/helpers/helpingFunctions.js
@@ -1,124 +1,113 @@
 const
-  fs = require('fs'),
-  path = require('path'),
-  models = require('./../../database/models');
+  fs = require('fs');
+const path = require('path');
+const models = require('../../database/models');
 
-var MemoryArray = JSON.parse(fs.readFileSync('./inputMemory.json', 'utf8'));
-var UniqueMemesNumber = 20;
+const MemoryArray = JSON.parse(fs.readFileSync('./inputMemory.json', 'utf8'));
+const UniqueMemesNumber = 20;
 
-function jsonAppender (jsonStr, image_id) {
-  console.log("=============================")
-  var obj = JSON.parse(jsonStr);
-  console.log(obj['image_id'])
-  console.log("=============================")
+function jsonAppender(jsonStr, imageId) {
+  console.log('=============================');
+  const obj = JSON.parse(jsonStr);
+  console.log(obj.imageId);
+  console.log('=============================');
 
-  obj['image_id'].push({"image_id":image_id});
-  console.log(obj)
-  jsonStr = JSON.stringify(obj)
-  return jsonStr
+  obj.imageId.push({ imageId });
+  console.log(obj);
+  jsonStr = JSON.stringify(obj);
+  return jsonStr;
 }
 
-module.exports =
-{
-  getArraySize: function () {
-    return MemoryArray.sentImages.length
+module.exports = {
+  getArraySize() {
+    return MemoryArray.sentImages.length;
   },
-  getPrint: function () {
-    return MemoryArray.sentImages
+  getPrint() {
+    return MemoryArray.sentImages;
   },
 
-  checkIfSentBefore: function (image_id, senderID) {
-    models.Memes_Sent.findOne({ where: { fb_id: senderID } }).then(function (record) {
+  checkIfSentBefore(imageId, senderID) {
+    models.Memes_Sent.findOne({ where: { fb_id: senderID } }).then((record) => {
       if (record) {
-        console.log("=============================")
+        console.log('=============================');
         // JSON.stringify(object)
-        console.log(JSON.parse(record.imgur_id_gallery))
-
-
-      }
-      else {
+        console.log(JSON.parse(record.imgur_id_gallery));
+      } else {
         return false;
       }
-
-    })
+    });
   },
-  sendImageToNewUser: function (image_id, senderID, image_type) {
-    var imgur_ids = new Array();
-    imgur_ids = imgur_ids.push({image_id});
-    console.log(imgur_ids)
+  sendImageToNewUser(imageId, senderID, imageType) {
+    let imgurIds = new Array();
+    imgurIds = imgurIds.push({ imageId });
+    console.log(imgurIds);
 
-    // tools.sendImageMessage(senderID, image_id);
+    // tools.sendImageMessage(senderID, imageId);
     models.Memes_Sent.create({
       fb_id: senderID,
-      imgur_id_gallery: JSON.stringify( imgur_ids)
-    }).then(function (record) {
+      imgur_id_gallery: JSON.stringify(imgurIds),
+    }).then((record) => {
       if (record) {
         return (record);
+      }
+      console.log('ERRRRRRRRRRRRRROR');
+    });
+  },
+
+  sendImageToExistingUser(imageId, senderID, imageType) {
+    models.Memes_Sent.findOne({ where: { fb_id: senderID } }).then((record) => {
+      if (record) {
+        imgurIds = imageType === 'gallery' ? jsonAppender(record.imgur_id_gallery, imageId) : jsonAppender(record.imgur_id_account, imageId);
+        models.Memes_Sent.update({
+          imgur_id_gallery: imgurIds,
+        }).then((record) => {
+          if (record) {
+            return (record);
+          }
+          console.log('ERRRRRRRRRRRRRROR');
+        });
       } else {
-        console.log("ERRRRRRRRRRRRRROR")
+        console.log('User should have been found !');
       }
     });
   },
 
-  sendImageToExistingUser: function (image_id, senderID, image_type) {
-    models.Memes_Sent.findOne({ where: { fb_id: senderID } }).then(function (record) {
-      if (record) {
-        imgur_ids = image_type == 'gallery' ? jsonAppender(record.imgur_id_gallery, image_id) : jsonAppender(record.imgur_id_account, image_id)
-        models.Memes_Sent.update({
-          imgur_id_gallery: imgur_ids
-        }).then(function (record) {
-          if (record) {
-            return (record);
-          } else {
-            console.log("ERRRRRRRRRRRRRROR")
-          }
-        })
-      }
-      else {
-        console.log("User should have been found !")
-      }
-
-    })},
-
-// if (!MemoryArray.sentImages.includes(ImageID)) {
-//   //Make sure to have UniqueMemes of certain number before resetting
-//   if (MemoryArray.sentImages.length == UniqueMemesNumber) {
-//     MemoryArray.sentImages = [];
-//     fs.writeFileSync('./inputMemory.json', JSON.stringify(MemoryArray, null, 2), 'utf-8');
-//   }
-//   //Add Image to Send Array
-//   MemoryArray.sentImages.push(ImageID);
-//   fs.writeFileSync('./inputMemory.json', JSON.stringify(MemoryArray, null, 2), 'utf-8');
-//   console.log("Added to array and Array length Now " + MemoryArray.sentImages.length)
-//   return false;
-//   //return false indicates that it wasn't sent before and send the link to the user
-// }
-// return true;
-getImageLink: function (data, i, counter) {
-      if (counter == -1) {
-        counter = 0;
-      }
-      if (data[i].is_album == true) {
-        console.log("link is " + data[i].images[counter].link)
-        return data[i].images[counter].link;
-      }
-      else {
-        console.log("link is " + data[i].link)
-        return data[i].link;
-      }
-    },
-      imageIDfetchr: function (ImageLink) {
-        var urlParts = /^(?:\w+\:\/\/)?([^\/]+)(.*)$/.exec(ImageLink);
-        ImageID = urlParts[2]; // /path/to/somwhere
-        if (path.extname(ImageID) == '.mp4') {
-          //Continue Cycling on different resposne if found a video to handle empty response of a video not supported in sending image API
-          console.log("Extension " + path.extname(ImageID) + "Is not supported ")
-        }
-        else {
-          console.log("Extension " + path.extname(ImageID) + "is supported ")
-        }
-        return ImageID;
-      },
+  // if (!MemoryArray.sentImages.includes(ImageID)) {
+  //   //Make sure to have UniqueMemes of certain number before resetting
+  //   if (MemoryArray.sentImages.length == UniqueMemesNumber) {
+  //     MemoryArray.sentImages = [];
+  //     fs.writeFileSync('./inputMemory.json', JSON.stringify(MemoryArray, null, 2), 'utf-8');
+  //   }
+  //   //Add Image to Send Array
+  //   MemoryArray.sentImages.push(ImageID);
+  //   fs.writeFileSync('./inputMemory.json', JSON.stringify(MemoryArray, null, 2), 'utf-8');
+  //   console.log("Added to array and Array length Now " + MemoryArray.sentImages.length)
+  //   return false;
+  //   //return false indicates that it wasn't sent before and send the link to the user
+  // }
+  // return true;
+  getImageLink(data, i, counter) {
+    if (counter == -1) {
+      counter = 0;
+    }
+    if (data[i].is_album == true) {
+      console.log(`link is ${data[i].images[counter].link}`);
+      return data[i].images[counter].link;
+    }
+    console.log(`link is ${data[i].link}`);
+    return data[i].link;
+  },
+  imageIDfetchr(ImageLink) {
+    const urlParts = /^(?:\w+\:\/\/)?([^\/]+)(.*)$/.exec(ImageLink);
+    ImageID = urlParts[2]; // /path/to/somwhere
+    if (path.extname(ImageID) == '.mp4') {
+      // Continue Cycling on different resposne if found a video to handle empty response of a video not supported in sending image API
+      console.log(`Extension ${path.extname(ImageID)}Is not supported `);
+    } else {
+      console.log(`Extension ${path.extname(ImageID)}is supported `);
+    }
+    return ImageID;
+  },
 
 
-}
+};
