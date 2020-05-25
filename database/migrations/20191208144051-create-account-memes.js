9ee95249af5a02e9df4618a2c88b97fdf9cce43a
Khaled <khaledbnmohamed@hotmail.com>
2020-05-25 06:39:01 +0200

    #10: remove temp drop table

--- database/migrations/20191208144051-create-account-memes.js
+++ database/migrations/20191208144051-create-account-memes.js
@@ -1,36 +1,28 @@
-'use strict';
-module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('AccountMemes', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      imgur_id: {
-        type: Sequelize.STRING
-      },
-      score: {
-        type: Sequelize.INTEGER,
-        defaultValue: 0,
-        allowNull: false,
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-     queryInterface.dropTable('AccountMemes');
-     queryInterface.dropTable('Account_Memes');
-     queryInterface.dropTable('Gallery_Memes');
-     return queryInterface.dropTable('Memes_Sents');
 
-  }
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('AccountMemes', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+    },
+    imgur_id: {
+      type: Sequelize.STRING,
+    },
+    score: {
+      type: Sequelize.INTEGER,
+      defaultValue: 0,
+      allowNull: false,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE,
+    },
+  }),
+  down: (queryInterface, Sequelize) => queryInterface.dropTable('AccountMemes'),
 };
